48b2ed0741de025f0c5ea90acfd62f7580d22747
inai17ibar <inai17ibar@gmail.com>
2017-08-05 21:22:25 +0900

    アプリアイコンとスプラッシュ画面の画像を追加した．リストビューの画像も仮置きした．

--- SightOnMock/FeedViewController.swift
+++ SightOnMock/FeedViewController.swift
@@ -23,15 +23,38 @@ class FeedViewController: ViewController, UITableViewDelegate, UITableViewDataSo
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20;
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "FeedListItem") as! FeedListItemTableViewCell
+            // タイトルの設定
             cell.title_name.text = "\(indexPath.row)"
+            // UIImage インスタンスの生成
+            let image:UIImage = UIImage(named:"sample")!
+            // 画像の幅・高さの取得
+            //var width = image.size.width
+            //var height = image.size.height
+            
+            // ImageView frame をCGRectで作った矩形に合わせる
+            //cell.photo.frame = rect;
+            
+            // UIImageView 初期化
+            cell.photo = UIImageView(image:image)
+            
+            // 画像の中心を画面の中心に設定
+            //imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+            
+            // UIImageViewのインスタンスをビューに追加
+            //self.view.addSubview(imageView)
+            
             return cell
         }
         return UITableViewCell()
