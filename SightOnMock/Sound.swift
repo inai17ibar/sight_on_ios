b1ea376da23f9226a181422a381ec6d5edddee15
inai17ibar <inai17ibar@gmail.com>
2017-08-07 17:04:54 +0900

    CocoaPodを導入した．Realmを入れてFeedで動作確認．AVFoundationによる音声ファイル再生も確認．
    注意：Podのプロジェクトが追加されたのでプロジェクトファイルが2つある，Projectは.workspaceから開くようにする

---
+++ SightOnMock/Sound.swift
@@ -0,0 +1,13 @@
+//
+//  Sound.swift
+//  SightOnMock
+//
+//  Created by inatani soichiro on 2017/08/06.
+//  Copyright © 2017年 inai17ibar. All rights reserved.
+//
+
+import UIKit
+
+class Sound: Object {
+
+}
---
+++ SightOnMock/realmDataSet.swift
@@ -0,0 +1,13 @@
+//
+//  realmDataSet.swift
+//  Pods
+//
+//  Created by inatani soichiro on 2017/08/06.
+//
+//
+
+import UIKit
+
+class realmDataSet: Object {
+
+}
