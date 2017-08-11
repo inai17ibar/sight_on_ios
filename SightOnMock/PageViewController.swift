b5597ba362bfcf4f65b966a6a9816b68b8280d1c
inai17ibar <inai17ibar@gmail.com>
2017-08-11 17:12:47 +0900

    PagaViewControllerを追加し，メインの2画面だけはスワイプで移動できるようにした．

---
+++ SightOnMock/PageViewController.swift
@@ -0,0 +1,78 @@
+//
+//  PageViewController.swift
+//  SightOnMock
+//
+//  Created by inatani soichiro on 2017/08/11.
+//  Copyright © 2017年 inai17ibar. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    let sboard: UIStoryboard? = UIStoryboard(name:"Main", bundle:nil)
+    var pageViewControllers: [UIViewController] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.view.backgroundColor = UIColor(patternImage:UIImage(named: "backBokashi.png")!)
+        //自分自身を指定
+        dataSource = self
+        
+        // Do any additional setup after loading the view.
+        let recordViewController: RecordViewController = sboard!.instantiateViewController(withIdentifier: "RecordViewController") as! RecordViewController
+        
+        let feedViewController: FeedViewController = sboard!.instantiateViewController(withIdentifier: "FeedViewController") as! FeedViewController
+        
+        //全ページを配列に格納
+        pageViewControllers = [recordViewController,feedViewController]
+        //UIPageViewControllerに表示対象を設定
+        setViewControllers([pageViewControllers[0]], direction: .forward, animated: false, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController:
+        UIPageViewController, viewControllerBefore viewController:UIViewController) -> UIViewController? {
+        //右にスワイプした場合に表示したいviewControllerを返す
+        //ようはページを戻す
+        //今表示しているページは何ページ目か取得する
+        let index = pageViewControllers.index(of: viewController)
+        if index == 0 {
+            //1ページ目の場合は何もしない
+            return nil
+        } else {
+            //1ページ目の意外場合は1ページ前に戻す
+            return pageViewControllers[index!-1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController:
+        UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        //左にスワイプした場合に表示したいviewControllerを返す
+        //ようはページを進める
+        //今表示しているページは何ページ目か取得する
+        let index = pageViewControllers.index(of: viewController)
+        if index == pageViewControllers.count-1 {
+            //最終ページの場合は何もしない
+            return nil
+        } else {
+            //最終ページの意外場合は1ページ進める
+            return pageViewControllers[index!+1]
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
